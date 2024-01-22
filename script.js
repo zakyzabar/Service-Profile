d396a24eda2591182260bad5baac400a7af6e3bb
zakyzabar <153757532+zakyzabar@users.noreply.github.com>
2024-01-22 09:47:01 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const text = document.querySelector(".text");
-const chars = text.textContent.split("");
-text.textContent = "";
-for (let i = 0; i < chars.length; i++) {
-  const span = document.createElement("span");
-  span.textContent = chars[i];
-  span.style.animationDelay = `${i * 0.1}s`;
-  text.appendChild(span);
-}
